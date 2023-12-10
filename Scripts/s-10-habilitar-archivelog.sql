621a08e561aca544dd4b8066b9636ebc65ecd367
JosmarSandoval <sandovalj5mr@gmail.com>
2023-12-10 17:29:12 -0600

    corrigiendo archivos s-09 y s-10. Nota: no olvidar compiar el control file que se crea en la fra en los otros destinos y cambiarles el nombre, este puede no tener el mismo nombre que el que coloqué

--- Scripts/s-10-habilitar-archivelog.sql
+++ Scripts/s-10-habilitar-archivelog.sql
@@ -8,9 +8,9 @@ Prompt creando pfile a partir de spfile
 create pfile form spfile
 
 Prompt configurando parametros para habilitar el modo archive
-alter system set log_archive_dest_1='/unam-bda/proyecto/d06/archivelogs/RASAPROY MANDATORY' scope=spfile;
+alter system set log_archive_dest_1='LOCATION=/unam-bda/proyecto/d06/archivelogs/RASAPROY MANDATORY' scope=spfile;
 alter system set log_archive_dest_2='LOCATION=USE_DB_RECOVERY_FILE_DEST MANDATORY'scope=spfile;
-alter system set log_archive_dest_3='/unam-bda/proyecto/d07/archivelogs/RASAPROY' scope=spfile;
+alter system set log_archive_dest_3='LOCATION=/unam-bda/proyecto/d07/archivelogs/RASAPROY' scope=spfile;
 alter system set log_archive_format='arch_rasaproy_%t_%s_%r.arc' scope=spfile;
 alter system set log_archive_min_succeed_dest=2 scope=spfile;
 
